8e633b2eff37f7f6864231e37de60d4c3cd99cdb
Celest90 <A880135553@my.uvm.edu.mx>
2026-01-03 18:36:16 -0800

    Inicio
    Página de inicio en la aplicación

---
+++ Inicio.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+
+st.set_page_config(page_title="Calculadora Estadistica Inferencial")
+
+
+st.title(":green[Aplicación Web Para Cálculo de Estadísticas Inferenciales]",
+         text_alignment ="center"
+         )
+
+st.subheader(':green[DESCRIPCIÓN]')
+st.markdown("*Esta aplicación web esta desarrollada como proyecto final "
+         "para la materia de 'Estadística Inferencial', con el objetivo de "
+         "facilitar el cálculo de diversas medidas estadísticas en un solo lugar.*",
+            text_alignment="justify"
+         )
